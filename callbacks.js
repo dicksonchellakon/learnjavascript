2c4b2710c637b75ba82fc7f3a4ca038a06a22d8b
dickson <dickson.bonaparte@gmail.com>
2023-01-26 15:45:02 +0000

    callbacks programes

---
+++ callbacks.js
@@ -0,0 +1,46 @@
+// Callbacks
+function greet(name, callbacks) {
+    console.log('Hi' + ' ' + name);
+    callbacks()
+}
+
+// callback function
+function callMe() {
+    console.log('I am callback function');
+}
+
+setTimeout(greet, 2000, 'John', callMe);
+setTimeout(greet, 2000, 'John', () => {
+    console.log('I am callback');
+});
+
+//greet('Peter', callMe); // Hi Peter
+
+
+
+//Nested Callbacks
+
+let someThing = (callback) => {
+    console.log("Do Something!");
+    callback();
+}
+
+let someThingElse  = (callback) => {
+  console.log("Do Something Else!");
+  callback();
+}
+
+let doThirdThing  = (callback) => {
+  console.log("Do Third thing!");
+  callback();
+}
+
+someThing(() => {
+  console.log("First anonymous callback!");
+  someThingElse(() => {
+    console.log("Second anonymous callback!")
+    doThirdThing(() => {
+      console.log("Third anonymous callback!");
+    })
+  })
+})
